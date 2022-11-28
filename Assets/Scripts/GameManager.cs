257d6a6ed84cf073c77ece704a39ab46e780b6a6
fvaldes0109 <fvaldes0109@gmail.com>
2022-11-27 22:57:53 -0500

    Progressive score and persistent high scores

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {   
 
     public GameObject endLevelText;
     public GameObject pressEnterText;
+    public GameObject enterName;
+    public GameObject scoresObject;
+    public GameObject scoresObjectList;
 
     public bool LevelEnded { get; private set; } = false;
+    public bool RoundEnded { get; private set; } = false;
+
     int currentPoints = 2;
+    int currentLevel = 1;
+
     PointsController pointsController;
 
     void Start() {
@@ -21,24 +30,44 @@ public class GameManager : MonoBehaviour
 
     void Update() {
 
+        if (RoundEnded) {
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                EnterScore();
+            }
+            return;
+        }
         if (LevelEnded && Input.GetKeyDown(KeyCode.Space)) {
             
             endLevelText.SetActive(false);
-            pressEnterText.SetActive(true);
+
+            currentLevel++;
+            if (currentLevel == 6) {
+
+                FinishRound();
+                return;
+            }
+
             LevelEnded = false;
+            pressEnterText.SetActive(true);
 
             pointsController.DespawnPoints();
             currentPoints = Mathf.Min(currentPoints + 1, 6);
             pointsController.SpawnPoints(currentPoints);
             GetComponent<TimerManager>().StartTimer();
         }
+
+        // OJO TEMPORAL
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            GetComponent<ScoreManager>().ClearScores();
+        }
     }
 
     public void NextLevel() {
         
+        if (LevelEnded) return;
+
         GetComponent<TimerManager>().StopTimer();
 
-        var pointsController = FindObjectOfType<PointsController>();
         var graphPolinomial = FindObjectOfType<GraphPolinomial>();
 
         Vector3[] mobilePoints = pointsController.MobilePoints;
@@ -52,7 +81,7 @@ public class GameManager : MonoBehaviour
             error += Mathf.Pow(point.y - graphPolinomial.CalculatePoints(point.x, polinomial).y, 2);
         }
         error /= staticPoints.Length;
-        // Score is arc cotangent of (error - 3)
+        // Score is arc cotangent of (error - 3) + 1
         float score = Mathf.Atan(1 / (error - 3)) + 1;
         if (error < 3) score += Mathf.PI;
 
@@ -61,5 +90,41 @@ public class GameManager : MonoBehaviour
         endLevelText.SetActive(true);
         pressEnterText.SetActive(false);
         LevelEnded = true;
+    }
+
+    void FinishRound() {
+
+        RoundEnded = true;
+        var scoreManager = GetComponent<ScoreManager>();
+
+        if (scoreManager.CheckIfHighScore()) {
+            enterName.SetActive(true);
+            enterName.GetComponentInChildren<TMP_InputField>().Select();
+        }
+        else ShowScoreList();
+    }
+
+    public void EnterScore() {
+            
+        GetComponent<ScoreManager>().SaveScore();
+        enterName.SetActive(false);
+        ShowScoreList();
+    }
+
+    void ShowScoreList() {
+        
+        var scoreManager = GetComponent<ScoreManager>();
+        var scoreList = scoreManager.Scores;
+
+        string text = "";
+        int i = 0;
+        foreach (var score in scoreList) {
+
+            text += $"{i + 1}. {score.Item1} - {score.Item2}\n";
+            i++;
+        }
+        
+        scoresObject.SetActive(true);
+        scoresObjectList.GetComponent<TextMeshProUGUI>().text = text;
     }
 }
